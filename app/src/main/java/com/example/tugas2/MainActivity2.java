33c480e9b4e75a6e940a343d824c27940e4bff01
Pentagon <pentagonbatlayar@gmail.com>
2021-03-10 18:24:02 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugas2/MainActivity2.java
@@ -0,0 +1,26 @@
+package com.example.tugas2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void toHome(View view) {
+        Intent intent = new Intent(MainActivity2.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void toArticle(View view) {
+        Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+        startActivity(intent);
+    }
+}
